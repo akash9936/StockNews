044486f71296b51fac90f7ab5b1073290937ec76
akashsingh <akashsingh@propelld.com>
2025-05-18 00:07:53 +0530

    Run without tacker for test

--- src/config/telegram.config.js
+++ src/config/telegram.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  botToken: process.env.TELEGRAM_BOT_TOKEN || '',
-  channelId: process.env.TELEGRAM_CHANNEL_ID || '',
+  botToken: process.env.BOT_TOKEN || process.env.TELEGRAM_BOT_TOKEN || '',
+  channelId: process.env.CHANNEL_ID || process.env.TELEGRAM_CHANNEL_ID || '',
   // Add any other Telegram-specific configuration here
 }; 
