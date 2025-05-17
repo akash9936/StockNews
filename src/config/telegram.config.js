742d28b8de22411f1252878c25412a8de5b36a2c
akashsingh <akashsingh@propelld.com>
2025-05-17 23:04:48 +0530

    Add GitHub Action for daily bot run

--- src/config/telegram.config.js
+++ src/config/telegram.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  botToken: process.env.TELEGRAM_BOT_TOKEN || '8069228318:AAH8JcGbvwkq7GSqHs3ZzkW1VCxWkpnN4JA',
-  channelId: process.env.TELEGRAM_CHANNEL_ID || '@GoStockLive',
+  botToken: process.env.TELEGRAM_BOT_TOKEN || '',
+  channelId: process.env.TELEGRAM_CHANNEL_ID || '',
   // Add any other Telegram-specific configuration here
 }; 
