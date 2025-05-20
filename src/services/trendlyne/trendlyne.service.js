7f49dbe203b6cd6d63b06477fce42f6f392ff625
akashsingh <akashsingh@propelld.com>
2025-05-20 22:42:42 +0530

    Disabled screen monitering

--- src/services/trendlyne/trendlyne.service.js
+++ src/services/trendlyne/trendlyne.service.js
@@ -144,8 +144,8 @@ class TrendlyneService extends BaseService {
    */
   async fetchAllInsights() {
     const [marketInsights, screenInsights] = await Promise.all([
-      this.fetchMarketInsights(),
-      this.fetchAllScreenInsights()
+      this.fetchMarketInsights()
+      //,this.fetchAllScreenInsights()  screen disable from hear
     ]);
 
     return [
